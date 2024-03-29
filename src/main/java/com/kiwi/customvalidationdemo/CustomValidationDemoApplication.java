9671453636e0ec6d2d586abf45c8b3d845a64518
pkilche <pkilche@ebates.com>
2024-03-29 00:42:30 -0400

    Initial Commit

---
+++ src/main/java/com/kiwi/customvalidationdemo/CustomValidationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.kiwi.customvalidationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomValidationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomValidationDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kiwi/customvalidationdemo/CustomValidationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiwi.customvalidationdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomValidationDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
